274af7b113cae1556b49a22d5ca50a8cca555799
StPVieira <pedrohq.vieira168@gmail.com>
2025-02-01 17:31:32 -0300

    feat: project pages and components files created

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Gestão Gen
+
+                    <div className='flex gap-4'>
+                        Login
+                        Sobre
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
