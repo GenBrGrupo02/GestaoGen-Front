d615b2ebf77b586206e97c788ec145e5a0f045ca
Lucas Henrique <lucas.silva01052@gmail.com>
2025-02-03 16:08:41 -0300

    Implementação do Footer, Navbar e Home.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { GithubLogo, InstagramLogo, XLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -6,19 +6,41 @@ function Footer() {
 
     return (
         <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Gestão Gen | Copyright: {data}
+            <footer className='bg-gray-800 px-4 md:px-16 lg:px-28 py-8'>
+                <div className='grid grid-cols-1 md:grid-cols-3'>
+                    <div>
+                        <h2 className='text-lg font-bold mb-4 text-white'>
+                            Contato
+                        </h2>
+                        <p className='text-gray-300'>
+                            São Paulo - SP <br /> +55 9999-9999 <br /> contato@gmail.com
                         </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                    </div>
+                    <div>
+                        <h2 className='text-lg font-bold mb-4 text-white'>
+                            Link Rápido
+                        </h2>
+                        <ul>
+                            <li><a href="#" className='hover:underline text-gray-300'>Home</a></li>
+                            <li><a href="#" className='hover:underline text-gray-300'>Sobre-nós</a></li>
+                            <li><a href="#" className='hover:underline text-gray-300'>Login</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2 className='text-lg font-bold mb-4 text-white'>
+                            Redes Sociais
+                        </h2>
+                        <ul className='flex space-x-4'>
+                            <li><GithubLogo className='text-gray-300' size={20} /><a href="#" className='hover:underline text-gray-300'>GitHub</a></li>
+                            <li><XLogo className='text-gray-300' size={20} /><a href="#" className='hover:underline text-gray-300'>Twitter</a></li>
+                            <li><InstagramLogo className='text-gray-300' size={20} /><a href="#" className='hover:underline text-gray-300'>Instagram</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
+                <div className='border-t border-gray-400 p-6 text-gray-300 text-center mt-6'>
+                <p>&copy; {data} GestãoGen. Todos os direitos reservados.</p>
+                </div>
+            </footer>
         </>
     )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
+import { List } from "lucide-react";
+import { AddressBook } from "@phosphor-icons/react";
 
 function Navbar() {
     const navigate = useNavigate();
@@ -13,24 +15,27 @@ function Navbar() {
     }
 
     return (
-        <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-            <div className="container flex justify-between text-lg">
-                <Link to="/home" className="font-bold hover:underline">Gestão Gen</Link>
-
-                <div className="flex gap-4">
-                    <Link to="/sobre" className="hover:underline">Sobre Nós</Link>
-
-                    {usuario.token ? (
-                        <>
-                            <Link to="/dashboard" className="hover:underline">Dashboard</Link>
-                            <button onClick={logout} className="hover:underline">Sair</button>
-                        </>
-                    ) : (
-                        <Link to="/login" className="hover:underline">Login</Link>
-                    )}
-                </div>
+        <header className="fixed top-0 left-0 w-full z-50 bg-transparent">
+            <div className="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 md:mx-auto md:flex-row md:items-center">
+                <a href="#" className="gap-2 flex items-center whitespace-nowrap text-2xl font-black"><AddressBook size={32} weight="fill" />
+                    <span className="text-black hover:text-accent">GestãoGen/Odonto</span>
+                </a>
+                <input type="checkbox" className="peer hidden" id="navbar-open" />
+                <label className="absolute top-5 right-7 cursor-pointer md:hidden" htmlFor="navbar-open">
+                    <span className="sr-only">Toggle Navigation</span>
+                    <List size={32} />
+                </label>
+                <nav aria-label="Header Navigation" className="peer-checked:mt-8 peer-checked:max-h-56 flex max-h-0 w-full flex-col justify-between overflow-hidden transition-all md:ml-24 md:max-h-full md:flex-row md:items-start">
+                    <ul className="flex flex-col items-center space-y-2 md:ml-auto md:flex-row md:space-y-0">
+                        <li className="text-gray-600 md:mr-12 hover:text-accent"><a className="transition-all duration-300" href="#faq">FAQ</a></li>
+                        <li className="text-gray-600 md:mr-12 hover:text-accent"><a href="#">Sobre-nós</a></li>
+                        <li className="text-gray-600 md:mr-12 hover:text-accent">
+                        <button className="rounded-full border-2 border-gray-600 px-6 py-1 font-medium text-gray-600 transition-colors hover:bg-gray-600 hover:text-white">Login</button>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-        </div>
+        </header>
     );
 }
 
