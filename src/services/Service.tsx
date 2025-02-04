19e37eb5e3eba9c31b1758bdd5f57df7f0525d44
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2025-02-04 12:26:36 -0300

    Preparando Deploy

--- src/services/Service.tsx
+++ src/services/Service.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import Usuario from "../models/Usuario"
 
 const api =axios.create({
-    baseURL: 'https://gestaogen-back.onrender.com/'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
