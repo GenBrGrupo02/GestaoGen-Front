722fc80f42579c11071187b46f08064fd57579ab
Bruna R Silva <brunarob97@gmail.com>
2025-02-04 11:11:05 -0300

    alterações dashboard

--- src/pages/clientes/Clientes.tsx
+++ src/pages/clientes/Clientes.tsx
@@ -10,19 +10,18 @@ function Clientes() {
     <h1 className="text-3xl font-bold">Clientes</h1>
 
     <div className="flex gap-4">
-                
                 <Link to="/clientes/form">
                     <span className="hidden sm:block">
                         <button
                             type="button"
-                            className="mr-1.5 px-3.5 py-2.5 inline-flex items-center rounded-md  bg-primary
-                text-sm font-semibold text-base-content shadow-xs ring-inset hover:bg-accent"
+                            className="flex items-center mr-1.5 px-3.5 py-2.5 rounded-md bg-accent text-black font-semibold shadow-lg hover:bg-success transition"
                         >
                             <UserPlus size={20} weight="fill" className="mr-1.5" />
                             Novo Cliente
                         </button>
                     </span>
                 </Link>
+                
                 </div>
                 </div>
             </div>
